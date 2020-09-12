5f03647b613afd9b7183efd4d66c696713788730
hieu2504 <hieu25042000@gmail.com>
2020-09-12 14:26:05 +0700

    commit

---
+++ TeduShop.Data/Repositories/PostRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IPostRepository
+    {
+
+    }
+    class PostRepository : RepositoryBase<Post>,IPostRepository
+    {
+        public PostRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
