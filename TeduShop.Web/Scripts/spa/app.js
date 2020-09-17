7618db8ae6d8dce239b77e371eb314781a20f769
hieu2504 <hieu25042000@gmail.com>
2020-09-17 13:46:11 +0700

    bai20

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myApp = angular.module('myModule', []);
+
+myApp.controller("schoolController", schoolController);
+
+myApp.directive("teduShopDirective", teduShopDirective);
+
+myApp.service('validatorService', validatorService);
+
+myController.$inject=['$scope','Validator']
+
+//declare: khai bao
+function schoolController($scope, validatorService) {
+    $scope.checkNumber = function () {
+        $scope.message = validatorService.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+}
+
+function validatorService($window) {
+    return {
+        checkNumber : checkNumber
+    }
+
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'this is even';
+        } else {
+            return 'this is odd';
+        }
+    }
+}
+
+function teduShopDirective() {
+    return {
+        templateUrl:"/Scripts/spa/teduShopDirective.html"
+    }
+}
